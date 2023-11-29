0507f00ee7bb86f073440756276a02d096514282
Romain Chardon <chardon.romain26@gmail.com>
2023-11-29 14:56:44 +0100

    Create dropdown.js

---
+++ dropdown.js
@@ -0,0 +1,38 @@
+makeElement(50);
+
+function makeElement(nbElement) {
+    const dropdown = document.querySelector('.dropdown-item');
+    console.log(dropdown);
+
+    for (let i = 0; i < nbElement; i++) {
+        const span = document.createElement('span');
+        span.setAttribute('onclick','selectElement(this);')
+        span.innerText = i + 'element'+ Math.floor(Math.random() * 500);
+
+        dropdown.appendChild(span);
+    }
+}
+
+function showDropdown() {
+    document.querySelector('.dropdown-content').classList.toggle("show");
+}
+
+function filterElement() {
+    const input = document.querySelector("#searchElement");
+    const filter = input.value.toUpperCase();
+
+    const element = document.querySelectorAll(".dropdown-item > span");
+    element.forEach((e) => {
+        const valueSpan = e.innerText;
+        if (valueSpan.toUpperCase().indexOf(filter) > -1) {
+            e.style.display = "";
+        } else {
+            e.style.display = "none";
+        }
+    })
+}
+
+function selectElement(element) {
+    const title = document.querySelector('.dropdown-title > span');
+    title.innerText = element.innerText;
+}
